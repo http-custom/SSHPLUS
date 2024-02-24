3c9327ac0ec43b0c365cb105d639cdd24ce2ba5b
http-custom <137656009+http-custom@users.noreply.github.com>
2024-02-24 15:00:58 -0300

    Add files via upload

---
+++ Modulos/menudp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+FECHA=$(date +"%Y-%m-%d")
+cor1='\033[1;31m'
+cor2='\033[0;34m'
+cor3='\033[1;35m'
+clear
+scor='\033[0m'
+echo -e "\E[44;1;37m       ELEGIR   UNA   OPCION      \E[0m"
+echo -e "[\033[1;36m 1:\033[1;31m] \033[1;37m• \033[1;32mINICIAR -REINICIAR Psi \033[1;31m"
+echo -e "[\033[1;36m 2:\033[1;31m] \033[1;37m• \033[1;33mINSTALAR PSIPHON 443 \033[1;31m    "
+echo -e "[\033[1;36m 3:\033[1;31m] \033[1;37m• \033[1;33mVER PUERTOS ACTIVOS \033[1;31m      \E[0m"
+echo  -e "[\033[1;36m 4:\033[1;31m] \033[1;37m• \033[1;33mVER CODIGO TARJET \033[1;31m  "
+echo  -e  "[\033[1;36m 5:\033[1;31m] \033[1;37m• \033[1;33mINSTALAR PSIPHON 80 \033[1;31m  "
+echo  -e  "[\033[1;36m 6:\033[1;31m] \033[1;37m• \033[1;33mPROBAR VELOCIDAD \033[1;31m  "
+echo  -e "[\033[1;36m 7:\033[1;31m] \033[1;37m• \033[1;33mLimpiar Ram \033[1;31m"
+echo  -e "[\033[1;36m 8:\033[1;31m] \033[1;37m• \033[1;33mBorrar Psiphon \033[1;31m "
+echo  -e "[\033[1;36m 9:\033[1;31m] \033[1;37m• \033[1;33mVer Conectados \033[1;31m "
+
+#leemos del teclado sentado
+read n
+
+case $n in
+        1) clear
+        wget https://raw.githubusercontent.com/http-custom/udpcustom/main/folder/udp-custom.sh -O install-udp && chmod +x install-udp && ./install-udp ''
+            echo -ne "\n\033[1;31mListo \033[1;33mPsiphon Iniciado o  \033[1;32mReiniciado!\033[0m"; read
+           ;;
+        2) clear
+        wget https://raw.githubusercontent.com/http-custom/udpcustom/main/folder/udp-custom.sh -O install-udp && chmod +x install-udp && ./install-udp ''
+           echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu2!\033[0m"; read 
+            ;;
+        3) clear
+            netstat -tnpl
+             sleep 6
+           ;; 
+        4) clear
+        cd /root/psi&&cat /root/psi/server-entry.dat;echo ''
+       echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu2!\033[0m"; read
+           ;;
+        5) cd /root && mkdir psi && cd /root/psi && wget https://raw.githubusercontent.com/Psiphon-Labs/psiphon-tunnel-core-binaries/master/psiphond/psiphond && chmod 777 psiphond && ./psiphond --ipaddress 0.0.0.0 --protocol FRONTED-MEEK-OSSH:80 generate && screen -dmS PSI ./psiphond run && cat /root/psi/server-entry.dat;echo ''
+        echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu2!\033[0m"; read
+         ;;
+        6) speedtest
+             sleep 6
+             ;;
+        7)     sync & sysctl -w vm.drop_caches=3 
+           menu2   ;;
+         8)  rm -rf /root/psi
+             menu2;;
+          9)  ./verconectados.sh
+        echo -ne "\n\033[1;31mEnter \033[1;33m Para volver al  \033[1;32mMenu2!\033[0m"; read
+             ;;
+        *) echo "OPCION INCORREPTA ";;
+esac
